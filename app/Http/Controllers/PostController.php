a55b628b85a8a1edf2532dc2673c6aa5e6eee26c
Eric Escobar Mendoza <eescobar@itsch.edu.mx>
2024-06-20 12:11:07 -0600

    Validaciones de crear y editar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post; //para poder hacer consultas a la base de datos
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -14,14 +15,58 @@ class PostController extends Controller
         ]);
     }
 
-    public function create()
+    public function create(Post $post)
     {
-        return view('posts.create');
+        return view('posts.create', compact('post'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts,slug',
+            'body' => 'required'
+        ], [
+            'title.required' => 'El campo título es obligatorio',
+            'slug.required' => 'El campo slug es obligatorio',
+            'slug.unique' => 'El campo slug debe ser único',
+            'body.required' => 'El campo contenido es obligatorio'
+        ]);
+
+        $post = $request->user()->posts()->create([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body
+        ]);
+
+        return redirect()->route('posts.edit', $post);
     }
 
     public function edit(Post $post)
     {
         return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required',
+            'slug' => 'required|unique:posts,slug,' . $post->id,
+            'body' => 'required'
+        ], [
+            'title.required' => 'El campo título es obligatorio',
+            'slug.required' => 'El campo slug es obligatorio',
+            'slug.unique' => 'El campo slug debe ser único',
+            'body.required' => 'El campo contenido es obligatorio'
+        ]);
+
+        $post->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body
+        ]);
+
+        return redirect()->route('posts.edit', $post);
     }
 
     public function destroy(Post $post)
